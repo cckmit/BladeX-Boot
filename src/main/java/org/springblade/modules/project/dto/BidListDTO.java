647184552c8385d5cfb919281286e9c13c180889
luchanghong <luch@ccscc.com.cn>
2021-10-25 10:16:30 +0800

    1.商机以及投标显示字段调整

--- src/main/java/org/springblade/modules/project/dto/BidListDTO.java
+++ src/main/java/org/springblade/modules/project/dto/BidListDTO.java
@@ -2,6 +2,8 @@ package org.springblade.modules.project.dto;
 
 import lombok.Data;
 
+import java.time.LocalDateTime;
+
 @Data
 public class BidListDTO {
 
@@ -12,4 +14,16 @@ public class BidListDTO {
 	private Integer Status;
 	private String recordName;
 	private String recordCode;
+	private String biddingType;
+	private String projectCatrgory;
+	private String industry;
+	private String major;
+	private String region;
+	private LocalDateTime tenderDate;
+	private String isRelationship;
+	private String contractAmount;
+	private String clientName;
+	private String clientType;
+	private String isWin;
+	private LocalDateTime winBidTime;
 }
